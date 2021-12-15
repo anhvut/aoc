52f4e63f2fe1c69e13467426ec3c6a121eedad75
Anh-Vu Tran <anhvutran1@gmail.com>
2021-12-15 13:15:26 +0100

    add dijkstra algorithm

--- src/adventOfCode2021/aoc2021-15.js
+++ src/adventOfCode2021/aoc2021-15.js
@@ -93,10 +93,49 @@ function aStar(start, goal, h, maze) {
   return INF;
 }
 
+const dijkstra = (start, goal, maze) => {
+  const ex = maze[0].length-1;
+  const ey = maze.length-1;
+  const INF = 100 * maze[0].length * maze.length;
+
+  function *findNeighbors(currentKey) {
+    const [x, y] = fromKey(currentKey);
+    if (x > 0) yield [x-1, y];
+    if (x < ex) yield [x+1, y];
+    if (y > 0) yield [x, y-1];
+    if (y < ey) yield [x, y+1];
+  }
+
+  const startKey = asKey(start);
+  const goalKey = asKey(goal);
+  const distanceTo = {[startKey]: 0};
+  const comeFrom = {};
+
+  const queue = [startKey];
+  while (queue.length > 0) {
+    const currentKey = queue.shift();
+    let distanceToCurrent = distanceTo[currentKey];
+    for (const neighbor of findNeighbors(currentKey)) {
+      const neighborKey = asKey(neighbor);
+      const d = maze[neighbor[1]][neighbor[0]];
+      const dTotal = distanceToCurrent + d;
+      if (dTotal < (distanceTo[neighborKey] ?? INF)) {
+        distanceTo[neighborKey] = dTotal;
+        comeFrom[neighborKey] = currentKey;
+        let i = 0;
+        while (i < queue.length && dTotal > distanceTo[queue[i]]) i++;
+        queue.splice(i, 0, neighborKey);
+      }
+    }
+  }
+  return reconstructPath(comeFrom, goalKey);
+};
+
 const part = (maze) => {
   const ex = maze[0].length-1;
   const ey = maze.length-1;
-  const path = aStar([0, 0], [ex, ey], ([x, y]) => maze[y][x] + (ex-x) + (ey-y), maze);
+  // const path = aStar([0, 0], [ex, ey], ([x, y]) => maze[y][x] + (ex-x) + (ey-y), maze);
+  const path = dijkstra([0, 0], [ex, ey], maze);
   let score = -maze[0][0];
   for (const p of path) {
     const [x, y] = fromKey(p);
