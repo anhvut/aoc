8cecf0df539aabb9650d75aa267e13e6f8b4ea35
Anh-Vu Tran <anhvutran1@gmail.com>
2021-12-26 03:42:55 +0100

    aoc2019-01 in typescript

---
+++ src/2019/aoc2019-01.ts
@@ -0,0 +1,20 @@
+export {};
+const fs = require('fs');
+const nbs: number[] = fs.readFileSync(__dirname + '/aoc2019-01.txt', 'utf-8').split(/\n/g).map(x => +x);
+
+const toFuel = (n: number): number => Math.floor(n / 3) - 2;
+
+const toTotalFuel = (n: number): number => {
+  let result = 0;
+  n = toFuel(n);
+  while (n > 0) {
+    result += n;
+    n = toFuel(n);
+  }
+  return result;
+}
+
+const part = (fct: (nb: number) => number): number => nbs.map(fct).reduce((a, b) => a + b);
+
+console.log(part(toFuel));
+console.log(part(toTotalFuel));
