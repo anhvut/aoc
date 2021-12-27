0772c9d804f3c826099abf492fb7a4b98c2611dc
Anh-Vu Tran <anhvutran1@gmail.com>
2021-12-27 18:57:45 +0100

    aoc2019-08

---
+++ src/2019/aoc2019-08.ts
@@ -0,0 +1,36 @@
+export {};
+const fs = require('fs');
+const width = 25;
+const height = 6;
+const size = width * height;
+const line: string[] = fs.readFileSync(__filename.replace(/\.js/, '.txt'), 'utf-8');
+
+function toChunks<T>(array: T[], size: number): T[][] {
+  const result: T[][] = [];
+  let pos = 0;
+  do {
+    const nextPos = pos + size;
+    result.push(array.slice(pos, nextPos));
+    pos = nextPos;
+  } while (pos < array.length);
+  return result;
+}
+
+const chunks: number[][] = toChunks(Array.from(line).map(x => +x), size);
+
+const part1 = (): number => {
+  const nbZero = chunks.map(chunk => chunk.reduce((a, b) => a + Number(b === 0), 0));
+  const [, index] = nbZero.reduce(([min, index], nb, i) => nb < min ? [nb, i] : [min, index], [Infinity, -1]);
+  const nb1 = chunks[index].reduce((a, b) => a + Number(b === 1), 0);
+  const nb2 = chunks[index].reduce((a, b) => a + Number(b === 2), 0);
+  return nb1 * nb2;
+};
+
+const part2 = (): void => {
+  const pixels = chunks[0].map((_, i) => chunks.find(chunk => chunk[i] !== 2)[i]);
+  const img = toChunks(pixels, width);
+  img.forEach((x => console.log(x.map(y => y ? '#' : ' ').join(''))));
+};
+
+console.log(part1());
+part2();
