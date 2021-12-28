bd6e10bb2799d91741b08c7dee2833f85c46cc2a
Anh-Vu Tran <anhvutran1@gmail.com>
2021-12-28 12:40:49 +0100

    aoc2019-11

---
+++ src/2019/aoc2019-11.ts
@@ -0,0 +1,165 @@
+export {};
+const fs = require('fs');
+const nbs: number[] = fs.readFileSync(__filename.replace(/\.js/, '.txt'), 'utf-8').split(',').map(x => +x);
+
+function *run(input: number[], program: number[]): Generator<number, number, number> {
+  let pos: number = 0;
+  let result: number = 0;
+  let relativeBase: number = 0;
+  const mem: number[] = program.slice();
+  const getValue = (mode: number, param: number) => (mode === 1 ? param : mode === 2 ? mem[relativeBase + param] : mem[param]) ?? 0;
+  const getDestinationAddress = (mode: number, param: number) => {
+    if (mode === 0) return param;
+    if (mode === 2) return relativeBase + param;
+    throw new Error(`Invalid destination mode ${mode}`);
+  }
+  while (pos < mem.length) {
+    const instruction = mem[pos];
+    const opcode = instruction % 100;
+    const param1Mode = Math.floor(instruction / 100) % 10;
+    const param2Mode = Math.floor(instruction / 1000) % 10;
+    const param3Mode = Math.floor(instruction / 10000) % 10;
+    switch (opcode) {
+      case 1:
+      case 2: {
+        const [src1, src2, dst] = mem.slice(pos + 1, pos + 4);
+        const value1 = getValue(param1Mode, src1);
+        const value2 = getValue(param2Mode, src2);
+        const value3 = getDestinationAddress(param3Mode, dst);
+        mem[value3] = opcode === 1 ? value1 + value2 : value1 * value2;
+        pos += 4;
+        break;
+      }
+      case 3: {
+        const dst = getDestinationAddress(param1Mode, mem[pos + 1]);
+        mem[dst] = input.shift();
+        pos += 2;
+        break;
+      }
+      case 4: {
+        const src = mem[pos + 1];
+        result = getValue(param1Mode, src);
+        pos += 2;
+        const inputNext = yield result;
+        if (inputNext != null) input.push(inputNext);
+        break;
+      }
+      case 5:
+      case 6: {
+        const [src1, src2] = mem.slice(pos + 1, pos + 3);
+        const value1 = getValue(param1Mode, src1);
+        const value2 = getValue(param2Mode, src2);
+        if ((opcode === 5 && value1) || (opcode === 6 && !value1)) pos = value2;
+        else pos += 3;
+        break;
+      }
+      case 7:
+      case 8: {
+        const [src1, src2, dst] = mem.slice(pos + 1, pos + 4);
+        const value1 = getValue(param1Mode, src1);
+        const value2 = getValue(param2Mode, src2);
+        const value3 = getDestinationAddress(param3Mode, dst);
+        mem[value3] = (opcode === 7 && value1 < value2) || (opcode === 8 && value1 === value2) ? 1 : 0;
+        pos += 4;
+        break;
+      }
+      case 9: {
+        const src = mem[pos + 1];
+        const adjust = getValue(param1Mode, src);
+        relativeBase += adjust;
+        pos += 2;
+        break;
+      }
+      case 99:
+        return result;
+      default:
+        throw Error(`Unknown opcode ${opcode} at position ${pos}`);
+    }
+  }
+}
+
+type Vector = number[];
+type Matrix = number[][];
+
+const serializePoint = ([x, y]: Vector): string => `${x}_${y}`;
+const deserializePoint = (s: string): Vector => s.split('_').map(Number);
+
+const getRotationMatrix = (angle: number, round?: boolean): Matrix => {
+  let cos = Math.cos(angle);
+  let sin = Math.sin(angle);
+  if (round) {
+    cos = Math.round(cos);
+    sin = Math.round(sin);
+  }
+  return [[cos, sin], [-sin, cos]];
+};
+
+const matrixMultiply = (m1: Matrix, m2: Matrix): Matrix => {
+  // m1 size (y1, x1) x m2 size (y2, x2) => size (y3=y1, x3=x2) - x1 must be equal y2
+  const result = [];
+  for (let yTarget = 0; yTarget < m1.length; yTarget++) {
+    const line = [];
+    result.push(line);
+    for (let xTarget = 0; xTarget < m2[0].length; xTarget++) {
+      let cell = 0;
+      for (let i = 0; i < m2.length; i++) {
+        cell += m1[yTarget][i] * m2[i][xTarget];
+      }
+      line.push(cell);
+    }
+  }
+  return result;
+};
+
+const matrixVectorMultiply = (m: Matrix, p: Vector): Vector => matrixMultiply([p], m)[0];
+
+const vectorAdd = (pt1, pt2) => pt1.map((x, i) => x + pt2[i]);
+
+function displayGrid(topLeft: number[], bottomRight: number[], map: Record<string, number>, position: Vector, direction: Vector) {
+  const grid: Matrix = Array(bottomRight[1] - topLeft[1] + 1).fill(0).map(() => Array(bottomRight[0] - topLeft[0] + 1).fill(-1));
+  for (const [key, value] of Object.entries(map)) {
+    const [x, y] = deserializePoint(key);
+    grid[y - topLeft[1]][x - topLeft[0]] = value;
+  }
+  const display: string[][] = grid.map(line => line.map(x => x === -1 ? ' ' : x === 0 ? '.' : '#'));
+  const directionKey = serializePoint(direction);
+  display[position[1] - topLeft[1]][position[0] - topLeft[0]] = {
+    '0_1': '^',
+    '1_0': '>',
+    '0_-1': 'v',
+    '-1_0': '<'
+  }[directionKey];
+  display.reverse().forEach(line => console.log(line.join('')));
+}
+
+const paint = (startColor: number): number => {
+  const rotateLeft = getRotationMatrix(Math.PI / 2, true);
+  const rotateRight = getRotationMatrix(-Math.PI / 2, true);
+  const generator = run([startColor], nbs);
+  let position: Vector = [0, 0];
+  let currentDirection: Vector = [0, 1]
+  const map: Record<string, number> = {};
+  map[serializePoint(position)] = startColor;
+  let topLeft: Vector = [0, 0];
+  let bottomRight: Vector = [0, 0];
+  for (;;) {
+    const currentColor = map[serializePoint(position)] || 0;
+    const color = generator.next(currentColor);
+    if (color.done) break;
+    map[serializePoint(position)] = color.value;
+    const direction = generator.next();
+    if (direction.done) break;
+    currentDirection = matrixVectorMultiply(direction.value ? rotateRight : rotateLeft, currentDirection);
+    position = vectorAdd(position, currentDirection);
+    topLeft = [Math.min(topLeft[0], position[0]), Math.min(topLeft[1], position[1])];
+    bottomRight = [Math.max(bottomRight[0], position[0]), Math.max(bottomRight[1], position[1])];
+  }
+  displayGrid(topLeft, bottomRight, map, position, currentDirection);
+  return Object.values(map).length;
+}
+
+const part1 = (): number => paint(0);
+const part2 = (): number => paint(1);
+
+console.log(part1())
+console.log(part2())
