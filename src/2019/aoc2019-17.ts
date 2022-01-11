5e4959dc70a5b01f7302dac4179856fdf998cc43
Anh-Vu Tran <anhvutran1@gmail.com>
2022-01-11 01:43:38 +0100

    add aoc2019-17

---
+++ src/2019/aoc2019-17.ts
@@ -0,0 +1,211 @@
+export {};
+const fs = require('fs');
+const nbs: number[] = fs.readFileSync(__filename.replace(/\.js/, '.txt'), 'utf-8').split(',').map(x => +x);
+
+function* run(input: number[], program: number[]): Generator<number, number, number[]> {
+  let pos: number = 0;
+  let result: number = 0;
+  let relativeBase: number = 0;
+  const mem: number[] = program.slice();
+  const getValue = (mode: number, param: number) => (mode === 1 ? param : mode === 2 ? mem[relativeBase + param] : mem[param]) ?? 0;
+  const getDestinationAddress = (mode: number, param: number) => {
+    if (mode === 0) return param;
+    if (mode === 2) return relativeBase + param;
+    throw new Error(`Invalid destination mode ${mode}`);
+  }
+  while (pos < mem.length) {
+    const instruction = mem[pos];
+    const opcode = instruction % 100;
+    const param1Mode = Math.floor(instruction / 100) % 10;
+    const param2Mode = Math.floor(instruction / 1000) % 10;
+    const param3Mode = Math.floor(instruction / 10000) % 10;
+    switch (opcode) {
+      case 1:
+      case 2: {
+        const [src1, src2, dst] = mem.slice(pos + 1, pos + 4);
+        const value1 = getValue(param1Mode, src1);
+        const value2 = getValue(param2Mode, src2);
+        const value3 = getDestinationAddress(param3Mode, dst);
+        mem[value3] = opcode === 1 ? value1 + value2 : value1 * value2;
+        pos += 4;
+        break;
+      }
+      case 3: {
+        const dst = getDestinationAddress(param1Mode, mem[pos + 1]);
+        mem[dst] = input.shift();
+        pos += 2;
+        break;
+      }
+      case 4: {
+        const src = mem[pos + 1];
+        result = getValue(param1Mode, src);
+        pos += 2;
+        const inputNext = yield result;
+        if (inputNext != null) input.push(...inputNext);
+        break;
+      }
+      case 5:
+      case 6: {
+        const [src1, src2] = mem.slice(pos + 1, pos + 3);
+        const value1 = getValue(param1Mode, src1);
+        const value2 = getValue(param2Mode, src2);
+        if ((opcode === 5 && value1) || (opcode === 6 && !value1)) pos = value2;
+        else pos += 3;
+        break;
+      }
+      case 7:
+      case 8: {
+        const [src1, src2, dst] = mem.slice(pos + 1, pos + 4);
+        const value1 = getValue(param1Mode, src1);
+        const value2 = getValue(param2Mode, src2);
+        const value3 = getDestinationAddress(param3Mode, dst);
+        mem[value3] = (opcode === 7 && value1 < value2) || (opcode === 8 && value1 === value2) ? 1 : 0;
+        pos += 4;
+        break;
+      }
+      case 9: {
+        const src = mem[pos + 1];
+        const adjust = getValue(param1Mode, src);
+        relativeBase += adjust;
+        pos += 2;
+        break;
+      }
+      case 99:
+        return result;
+      default:
+        throw Error(`Unknown opcode ${opcode} at position ${pos}`);
+    }
+  }
+}
+
+function getMap() {
+  const map: string[][] = [[]];
+  const generator = run([], nbs);
+  for (;;) {
+    const next = generator.next();
+    if (next.done) break;
+    if (next.value === 10) map.push([]);
+    else map[map.length - 1].push(String.fromCharCode(next.value));
+  }
+  return map;
+}
+
+const part1 = (): number => {
+  const map = getMap();
+  map.forEach(line => console.log(line.join('')));
+  let result = 0;
+  for (let y = 0; y < map.length; y++)
+    for (let x = 0; x < map[y].length; x++)
+      if (map[y][x] === '#' && map[y][x-1] === '#' && map[y][x+1] === '#' && map[y+1]?.[x] === '#' && map[y-1]?.[x] === '#')
+        result += x * y;
+  return result;
+};
+
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left
+}
+
+type Vector = number[];
+type Matrix<T = number> = T[][];
+
+const oppositeDirection = (dir: Direction): Direction => (dir + 2) % 4;
+const getDirectionVector = (dir: Direction): Vector => [[0, -1], [1, 0], [0, 1], [-1, 0]][dir];
+const vectorAdd = (pt1: Vector, pt2: Vector) => pt1.map((x, i) => x + pt2[i]);
+
+function findPosition<T>(map: Matrix<T>, search: T): Vector {
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[0].length; x++) {
+      if (map[y][x] === search) return [x, y];
+    }
+  }
+}
+
+function findNextDirection(map: Matrix<string>, position: Vector, forbiddenDirection: Direction): Direction {
+  for (let d = 0; d < 4; d++) {
+    if (d === forbiddenDirection) continue;
+    const [x, y] = vectorAdd(position, getDirectionVector(d));
+    if (map[y]?.[x] === '#') return d;
+  }
+  return null;
+}
+
+function findNextDirectionInstructions(from: Direction, to: Direction): string[] {
+  if (from === to) return [];
+  if ((from + 1) % 4 === to) return ['R'];
+  if (from === (to + 1) % 4) return ['L'];
+  return ['R', 'R'];
+}
+
+function advanceDirection(map: Matrix<string>, position: Vector, direction: Direction): [number, Vector] {
+  const directionVector = getDirectionVector(direction);
+  let count = 0;
+  for (;;) {
+    const nextPosition = vectorAdd(position, directionVector);
+    if (map[nextPosition[1]]?.[nextPosition[0]] !== '#') return [count, position];
+    count++;
+    position = nextPosition;
+  }
+}
+
+function *findAllRoutines(input: string): Generator<[string, Record<string, string>]> {
+  // NB: J is 9th alphabet character = last possible function name
+  const instructions = input + ',';   // simplification
+  function *findRoutines(instructions: string, inputFunctions: Record<string, string>, nextFunctionName: string): Generator<[string, Record<string, string>]> {
+    if (instructions.match(/^[A-J,]+$/)) {
+      if (instructions.length <= 21) yield [instructions.replace(/,$/, ''), inputFunctions];
+      return;
+    }
+    const nextNextFunctionName = String.fromCharCode(nextFunctionName.charCodeAt(0) + 1);
+    let position = 0;
+    while ((instructions[position] ?? '').match(/[A-J,]/)) position++;
+    if (position >= instructions.length) return;
+    for (let length = 20; length > 0; length--) {
+      if (instructions[position + length] === ',' && instructions[position + length - 1].match(/\d/)) {
+        const search = instructions.slice(position, position + length + 1);
+        if (search.match(/[A-J]/)) continue;  // subroutine included, skip
+        const functions = {...inputFunctions, [nextFunctionName]: search.slice(0, -1)};
+        const newInstructions = instructions.replaceAll(search, `${nextFunctionName},`);
+        yield *findRoutines(newInstructions, functions, nextNextFunctionName);
+      }
+    }
+  }
+  yield *findRoutines(instructions, {}, 'A');
+}
+
+function findInstructions(map: string[][]): Array<string|number> {
+  let direction = Direction.Up;
+  let position = findPosition(map, '^');
+  let forbiddenDirection = null;
+  let nextDirection;
+  const instructions: Array<string|number> = [];
+  while ((nextDirection = findNextDirection(map, position, forbiddenDirection)) != null) {
+    instructions.push(...findNextDirectionInstructions(direction, nextDirection));
+    direction = nextDirection;
+    forbiddenDirection = oppositeDirection(direction);
+    let [count, nextPosition] = advanceDirection(map, position, direction);
+    instructions.push(count);
+    position = nextPosition;
+  }
+  return instructions;
+}
+
+const part2 = (): number => {
+  const map: Matrix<string> = getMap();
+  const instructions = findInstructions(map);
+  const allRoutines = [...findAllRoutines(instructions.join(','))];
+  const [main, routines] = allRoutines.find(([, r]) => Object.values(r).length <= 3);
+  console.log(main, routines);
+  const inputString = main + '\n' + Object.keys(routines).sort().map(k => routines[k] + '\n').join('') + 'y\n';
+  const input = Array.from(inputString).map(x => x.charCodeAt(0));
+  let lastOutput;
+  const nbs2 = nbs.slice();
+  nbs2[0] = 2;
+  for (const o of run(input, nbs2)) lastOutput = o;
+  return lastOutput;
+};
+
+console.log(part1());
+console.log(part2());
