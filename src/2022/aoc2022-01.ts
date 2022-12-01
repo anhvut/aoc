dd0eb439902a62599a6d42daf5928c6631fdf1b3
Anh-Vu Tran <anhvutran1@gmail.com>
2022-12-01 09:33:32 +0100

    add aoc2022-01

---
+++ src/2022/aoc2022-01.ts
@@ -0,0 +1,16 @@
+export {};
+const fs = require('fs');
+const nbs: number[] = fs.readFileSync(__dirname + '/aoc2022-01.txt', 'utf-8').split(/\n/g).reduce((agg, line) => {
+  line ? agg[agg.length - 1] += +line : agg.push(0);
+  return agg;
+}, [0]);
+
+const part1 = (n = nbs) => n.reduce((a, b) => Math.max(a, b));
+
+const part2 = () => {
+  const nbs2 = nbs.slice().sort((a, b) => b - a);
+  return nbs2[0] + nbs2[1] + nbs2[2];
+}
+
+console.log(part1());
+console.log(part2());
