63be8c7b84ef3304a6e437a9f84f58baf441f0ba
Anh-Vu Tran <anhvutran1@gmail.com>
2022-12-18 12:00:41 +0100

    add template

---
+++ src/2022/template.ts
@@ -0,0 +1,28 @@
+export {};
+
+const parse = (input: string[]) => {
+  return input;
+}
+
+const part1 = (input: string[]) => {
+  const lines = parse(input);
+  return 0;
+};
+
+const part2 = (input: string[]) => {
+  const lines = parse(input);
+  return 0;
+};
+
+const inputSample = `
+
+`.split('\n');
+
+const inputReal = `
+
+`.split('\n');
+
+console.log(part1(inputSample));
+//console.log(part1(inputReal));
+//console.log(part2(inputSample));
+//console.log(part2(inputReal));
